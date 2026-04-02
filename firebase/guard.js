d8a9c4fe59c4c529c563ddbe9f84496fc311a96e
Anupam-Kumar-Pandey <anupamkp123@gmail.com>
2026-04-02 10:58:57 +0530

    Delete firebase directory

--- firebase/guard.js
+++
@@ -1,9 +0,0 @@
-import { onUserChange } from "./auth.js";
-
-export function protectPage() {
-  onUserChange((user) => {
-    if (!user) {
-      window.location.href = "./login.html";
-    }
-  });
-}
